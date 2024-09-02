7ba833f3aff0113d9c446a4e79e8cc0b834769d8
Quantum <Quantuman134@gmail.com>
2024-09-02 16:53:35 +0800

    import tensor image: specify dtype, fix resize bug

--- PyToolsTest.py
+++ PyToolsTest.py
@@ -1,7 +1,11 @@
 import PyTools.PytorchTools as PT
+import torch
 
 if __name__ == "__main__":
     device = PT.torch_device_config()
     img_tensor = PT.import_image_tensor(img_dir="./TestSets/paw.png", device=device)
     print(img_tensor.size())
+    img_tensor = PT.import_image_tensor(img_dir="./TestSets/paw.png", device=device, size=(512, 512), dtype=torch.float64)
+    print(img_tensor.size())
+    print(img_tensor.dtype)
     #PT.export_image_tensor(img_tensor, "./TestSets/test.png")
